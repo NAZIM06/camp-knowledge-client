0f1640e99bdd5f9b780f626d85a006c600ca0bb6
unknown <anikm2982@gmail.com>
2023-06-08 18:53:43 +0600

    login updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,61 +1,57 @@
-/* eslint-disable no-unused-vars */
 import React, { useContext, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import { AuthContext } from '../provider/AuthProvider';
 import Loader from './Shared/Loader';
+import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 
 const Login = () => {
     const { signInUser, loading, setLoading, googleSignInUser, setUser } = useContext(AuthContext);
-    const [error, setError] = useState('')
-    const navigate = useNavigate();
-    const location = useLocation();
-    const from = location.state?.from?.pathname || '/'
+    const [error, setError] = useState('');
+    const [show, setShow] = useState(false); // Updated initial state to false
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
+    const onSubmit = data => {
+        setError('');
 
-        setError('')
-        if (password.length < 6) {
-            setError('Password at least 6 character long')
-            return;
-        } else {
-            signInUser(email, password)
-                .then(result => {
-                    const loggedUser = result.user;
-                    setLoading(false)
-                    navigate(from, { replace: true })
-                    console.log(loggedUser)
-                })
-                .catch(error => {
-                    const errorMessage = error.message;
-                    if (errorMessage === 'Firebase: Error (auth/wrong-password).') {
-                        setError('Password or Email invalid')
-                        setLoading(false)
-                    }
-                    console.log(errorMessage)
-                })
-        }
-        // console.log(event.target.email.value)
-    }
+        signInUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                setLoading(false);
+            })
+            .catch(err => {
+                const errorMessage = err.message;
+                if (errorMessage === 'Firebase: Error (auth/invalid-email).') {
+                    setError('Please Input a valid email address');
+                    setLoading(false);
+                } else if (errorMessage === 'Firebase: Error (auth/wrong-password).') {
+                    setError('wrong password. Please try again');
+                    setLoading(false);
+                }
+
+                console.log(errorMessage);
+            });
+    };
 
     const handleGoogleSignIn = () => {
-        googleSignInUser()
-            .then(result => {
-                const loggedUser = result.user;
-                setUser(loggedUser)
-                setLoading(false)
-                navigate(from, { replace: true })
-            })
-            .catch(error => {
-                const errorMessage = error.message;
-                console.log(errorMessage)
-            })
-    }
+        if (!show) { // Prevent executing when the eye icon is clicked
+            googleSignInUser()
+                .then(result => {
+                    setUser(result.user);
+                    setLoading(false);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                    setLoading(false);
+                });
+        }
+    };
+
+    const togglePasswordVisibility = () => {
+        setShow(!show);
+    };
 
     return (
         <>
@@ -63,19 +59,22 @@ const Login = () => {
                 loading && <Loader />
             }
             <div className='mx-auto p-10 w-full sm:w-5/6 md:w-4/6 lg:w-3/6 xl:w-2/6'>
-                <form onSubmit={handleSubmit} className='p-10 bg-base-200 rounded-xl border-2'>
-                    <p className='text-xl font-bold text-black mb-7'>Login</p>
-                    <div className="flex flex-col mb-4">
-                        <label className="text-black font-semibold py-3" htmlFor="email">Username or Email</label>
-                        <input className="outline-none border-2 border-gray-300 rounded-md p-2 focus:border-gray-500" type="email" name="email" required placeholder='Username or Email' />
+            <form className='p-10 bg-base-200 rounded-xl border-2' onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col mb-4"> <label className='text-xl font-semibold mb-3'>Email</label>
+                    <input placeholder='Email' className='mb-5 p-3 focus:outline-none' {...register('email', { required: true })} />
+                    {errors?.email?.type === 'required' && <p className='text-red-800 mb-2'>This field is required</p>}</div>
+                   
+                    <label className='text-xl font-semibold mb-3'>Password</label>
+                    <div className='relative w-full'>
+                        <input placeholder='Password' className='mb-5 w-full p-3 focus:outline-none' type={show ? 'text' : 'password'} {...register('password', { required: true })} />
+                        <div onClick={togglePasswordVisibility} className='absolute inset-y-0 right-3 top-3.5'>
+                            {show ? <FaEye className='w-5 h-5' /> : <FaEyeSlash className='w-5 h-5' />}
+                        </div>
                     </div>
-                    <div className="flex flex-col mb-4">
-                        <label className="text-black font-semibold py-3" htmlFor="email">Password</label>
-                        <input className="outline-none border-2 border-gray-300 rounded-md p-2 focus:border-gray-500" type="password" name="password" required placeholder='Password' />
-                    </div>
-                    <p className='text-red-700 mt-3'>{error}</p>
-                    <button className='button'>Login</button>
-                    <p className='text-center p-3'>Don't have an account? <Link to='/register'><span className='text-btn-color underline'>Create Account</span></Link></p>
+                    {errors?.password?.type === 'required' && <p className='text-red-800 mb-2'>This field is required</p>}
+                    <p className='text-center p-3'>Don't have an account? <Link to='/register'><span className='text-btn-color underline'>Register</span></Link></p>
+                    <p className='text-red-800 py-3'>{error}</p>
+                    <button type='submit' className='button mx-auto flex justify-center w-full'>Login</button>
                 </form>
                 <div onClick={handleGoogleSignIn} className='my-5 flex w-10/12 sm:w-full rounded-3xl mx-auto p-2 justify-between items-center hover:bg-[#121212] hover:text-white cursor-pointer border-2'>
                     <FcGoogle className='h-6 w-6' />
