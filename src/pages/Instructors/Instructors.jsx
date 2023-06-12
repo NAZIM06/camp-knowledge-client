92aad18c48b1ec69aa836bb6c7349999b88f2ade
unknown <anikm2982@gmail.com>
2023-06-12 21:32:29 +0600

    isntructor error edited

--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -16,19 +16,24 @@ const Instructors = () => {
             <Helmet>
                 <title>CampKnowledge || Instructors</title>
             </Helmet>
-            <div className='w-full'>
-            <p className='mb-12 text-2xl text-center font-bold'>All Instructors</p>
-                <div className='grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-5 mx-auto'>
-                    {
-                        allInstructors.map(instructor =>
-                            <div key={instructor._id} className="card w-80 glass shadow-2xl group">
-                                <figure><img className='h-64 w-full object-cover' src={instructor?.image} alt={instructor.name} /></figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">{instructor.name}</h2>
-                                    <p className='font-semibold'>Email: <span className='font-normal'>{instructor.email}</span></p>
+            <div className='w-full flex justify-center'>
+                <div className='max-w-screen-lg'>
+                    <p className='mb-12 text-2xl text-center font-bold'>All Instructors</p>
+                    <div className='grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-5 mx-auto'>
+                        {
+                            allInstructors.map(instructor =>
+                                <div key={instructor._id} className="card w-full sm:w-80 glass shadow-2xl group">
+                                    <figure>
+                                        <img className='h-64 w-full object-cover' src={instructor?.image} alt={instructor.name} />
+                                    </figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title">{instructor.name}</h2>
+                                        <p className='font-semibold'>Email: <span className='font-normal'>{instructor.email}</span></p>
+                                    </div>
                                 </div>
-                            </div>)
-                    }
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         </>
